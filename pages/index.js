01992ed0679ed1a5a0c633402e0ff942d3174ee0
hypocriticism1 <130147561+hypocriticism1@users.noreply.github.com>
2024-01-26 17:41:38 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import mainstyles from './mainstyle.module.css'
 
 export default function Home() {
   return (
+    <div>
       <Head>
         <title>고물이의 게임공장</title>
         <link rel="icon" href="/favicon.ico" />
@@ -82,5 +83,6 @@ export default function Home() {
             </div>
         </div>
       </main>
+    </div>
   )
 }
