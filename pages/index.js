52611a9a10f43ca8f2d24fc7a44a3b099834b02c
hypocriticism1 <130147561+hypocriticism1@users.noreply.github.com>
2024-01-26 17:21:07 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import mainstyles from './mainstyle.css'
+import mainstyles from './mainstyle.module.css'
 
 export default function Home() {
   return (
