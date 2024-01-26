80138ad4748b36473479468691f760ea78be7a8f
hypocriticism1 <130147561+hypocriticism1@users.noreply.github.com>
2024-01-26 17:58:38 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
       </Head>
       <main>
         <div className={mainstyles.homepageart}>
-            <img src="/images/homepageart.jpg"></img>
+            <img src="/images/homepageart.jpg" alt="홈페이지 상단 이미지"></img>
         </div>
         <div className={mainstyles.profileimage}>
-            <img src="/images/profile.jpg"></img>
+            <img src="/images/profile.jpg" alt="프로필 이미지"></img>
         </div>
         <h2 className={mainstyles.head_2}>고물이의 게임공장 홈페이지</h2>
         <div className={mainstyles.abouttext}>
@@ -29,55 +29,55 @@ export default function Home() {
         <div className={mainstyles.gamecontent}>
             <div>
                 <div>
-                    <img src="/images/game_1_cover.jpg"></img>
+                    <img src="/images/game_1_cover.jpg" alt="게임 이미지 1"></img>
                 </div>
                 <p>Clockwork Gear Puzzle</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/game_2_cover.jpg"></img>
+                    <img src="/images/game_2_cover.jpg" alt="게임 이미지 2"></img>
                 </div>
                 <p>유레카! 초전도체 연구소</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/underconstruction.jpg"></img>
+                    <img src="/images/underconstruction.jpg" alt="게임 이미지 3"></img>
                 </div>
                 <p>화살비</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/underconstruction.jpg"></img>
+                    <img src="/images/underconstruction.jpg" alt="게임 이미지 4"></img>
                 </div>
                 <p>뷰튜브 알고리즘 탐험</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/underconstruction.jpg"></img>
+                    <img src="/images/underconstruction.jpg" alt="게임 이미지 5"></img>
                 </div>
                 <p>양자멍</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/underconstruction.jpg"></img>
+                    <img src="/images/underconstruction.jpg" alt="게임 이미지 6"></img>
                 </div>
                 <p>꽃잎모으기</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/underconstruction.jpg"></img>
+                    <img src="/images/underconstruction.jpg" alt="게임 이미지 7"></img>
                 </div>
                 <p>찐따 시뮬레이터</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/underconstruction.jpg"></img>
+                    <img src="/images/underconstruction.jpg" alt="게임 이미지 8"></img>
                 </div>
                 <p>고물이의 암산교실</p>
             </div>
             <div>
                 <div>
-                    <img src="/images/underconstruction.jpg"></img>
+                    <img src="/images/underconstruction.jpg" alt="게임 이미지 9"></img>
                 </div>
                 <p>미정</p>
             </div>
