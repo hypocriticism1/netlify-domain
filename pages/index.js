3d0f75aec562e52ec8fadb3113be62c7545cf325
hypocriticism1 <130147561+hypocriticism1@users.noreply.github.com>
2024-01-26 17:04:40 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,18 @@ export default function Home() {
       <Head>
         <title>고물이의 게임공장</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-<html>
-    <head>
         <link href="https://hangeul.pstatic.net/hangeul_static/css/nanum-square.css" rel="stylesheet"></link>
         <link rel="stylesheet" type="text/css" href="mainstyle.css"></link>
-    </head>
-    <div>
-        <img class="homepageart" src="/images/homepageart.jpg"></img>
-    </div>
-    <h2 class="head_2">고물이의 게임공장 홈페이지</h2>
-    <div class="abouttext">
-        <p>여기는 게임개발봇 고물이가 운영하는 홈페이지입니다.</p>
-        <p>지금까지 고물이가 만든 게임과 앞으로 만들 게임을 게시하는 곳입니다.</p>
-    </div>
-</html>
+      </Head>
+      <main>
+        <div>
+            <img class="homepageart" src="/images/homepageart.jpg"></img>
+        </div>
+        <h2 class="head_2">고물이의 게임공장 홈페이지</h2>
+        <div class="abouttext">
+            <p>여기는 게임개발봇 고물이가 운영하는 홈페이지입니다.</p>
+            <p>지금까지 고물이가 만든 게임과 앞으로 만들 게임을 게시하는 곳입니다.</p>
+        </div>
       </main>
     
     </div>
