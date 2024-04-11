06870c03240950a1055319372ac5a29a805d48e1
hypocriticism1 <130147561+hypocriticism1@users.noreply.github.com>
2024-04-11 22:00:53 +0900

    Update eureka_superconductor_lab.js

--- pages/eureka_superconductor_lab.js
+++ pages/eureka_superconductor_lab.js
@@ -1,1 +1,11 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import mainstyles from './mainstyle.module.css'
 
+export default function Home() {
+  return (
+
+    
+  )
+}
