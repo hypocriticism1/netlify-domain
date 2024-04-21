262027f013924071416cde21e606d45f53aadc21
hypocriticism1 <130147561+hypocriticism1@users.noreply.github.com>
2024-04-21 10:15:04 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,6 @@ import mainstyles from './mainstyle.module.css'
 
 export default function Home() {
   return (
-    <head>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11431315721"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'AW-11431315721');
-      </script>
-    </head>
     
     <div>
       <Head>
